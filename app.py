3f33967f3f7164f10764cd8a6506fa6233ed5006
eagomezc <eagomezc@unal.edu.co>
2018-01-24 12:53:16 +0000

    Update

--- app.py
+++ app.py
@@ -1,26 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    return render_template('home.html')
-
-@app.route('/database')
-def hello():
+def index():
+    return render_template('index.html')
+  
+@app.route('/database.html')
+def database():
     return render_template('database.html')
+
+@app.route('/search.html')
+def search():
+    return render_template('search.html')
+
+@app.route('/results/<query>')
+def results(query):
+    return render_template('results.html')
 
 @app.route('/ind/<individual>')
 def individual(individual):
     return render_template('individual.html')
 
-@app.route('/tools')
-def tool():
+@app.route('/tools.html')
+def tools():
     return render_template('tools.html')
 
-@app.route('/help')
+@app.route('/help.html')
 def help():
     return render_template('help.html')
 
