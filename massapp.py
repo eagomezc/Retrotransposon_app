8a5afa9cc19555db14801b30f8ead28ca28a1c59
eagomezc <31103784+eagomezc@users.noreply.github.com>
2018-02-13 17:26:17 +0000

    Update massapp.py

--- massapp.py
+++ massapp.py
@@ -6,18 +6,6 @@ def mzidentml(fil):
         sequences = [item['SpectrumIdentificationItem'][0]['PeptideSequence'] for item in f]
     return sequences
 
-def mzidentml(fil):
-    pattern = r"<PeptideSequence>([A-Z]+)"
-    mz = open(fil,"r")
-    lines=mz.readlines()
-    peptides=[]
-    for l in lines:
-        if "<PeptideSequence>" in l:
-            rgx=re.findall(pattern,l)
-            if len(rgx)!=0: 
-                peptide="".join(rgx)
-                peptides.append(peptide)
-    return peptides
 
 def mztab(fil):
     pattern = r"PSM\s([A-Z]+)"
